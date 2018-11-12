18db21f2edf7be080e69679a08b2706f1e7ef1aa
snikshitha <sreehari.sreddy@gmail.com>
2018-11-12 20:30:03 +0800

    tutorial and example added

--- src/main/java/com/cas/app/App.java
+++ src/main/java/com/cas/app/App.java
@@ -1,5 +1,10 @@
 package com.cas.app;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,13 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
         System.out.println( "Hello World!" );
+        Cluster cluster = Cluster.builder().addContactPoints("192.168.31.255").build();
+        Session session = cluster.newSession();
+        ResultSet result = session.execute("select time,logitude,latitude from vehicle_tracker.location");
+        for (Row row : result) {
+			System.out.println(row.getDate("time")+"\t"+row.getDouble("longitude")+"\t"+row.getDouble("latitude"));
+		}
     }
 }
